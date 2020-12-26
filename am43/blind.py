62708960f43bc20460847c907a20ab8e4cdd518e
breitburg <me@breitburg.com>
2020-12-27 01:50:52 +0300

    Improved internal library components

--- am43/blind.py
+++ am43/blind.py
@@ -1,14 +1,14 @@
 from bluepy import btle
 from munch import Munch
-import time
+from time import sleep
 
 
 class Blind:
     def __init__(self, device: object = None, auto_connect: bool = True, retry: bool = True):
-        self.retry = retry
-        self.device = device
+        self._retry = retry
         self.identifiers = Munch(move=0x0d, stop=0x0a, battery=0xa2, light=0xaa, position=0xa7)
-        self.blind = None
+        self._device = device
+        self._peripheral = None
         self._battery = self._position = self._light = None
 
         if auto_connect:
@@ -17,42 +17,42 @@ class Blind:
     def connect(self, service: str = 'fe50', characteristic: str = 'fe51') -> None:
         while True:
             try:
-                self.blind = btle.Peripheral(self.device)
+                self._peripheral = btle.Peripheral(self._device)
 
                 delegate = btle.DefaultDelegate()
                 delegate.handleNotification = lambda handle, data: self._update_data(data=data)
-                self.blind.setDelegate(delegate)
+                self._peripheral.setDelegate(delegate)
 
-                self.characteristic = self.blind.getServiceByUUID(service).getCharacteristics(characteristic)[0]
+                self.characteristic = self._peripheral.getServiceByUUID(service).getCharacteristics(characteristic)[0]
                 return
             except btle.BTLEDisconnectError as error:
                 self.disconnect()
-                if not self.retry:
+                if not self._retry:
                     raise error
                 else:
-                    time.sleep(1)
-    
+                    sleep(1)
+
     def disconnect(self):
-        if self.blind:
-            self.blind.disconnect()
-            self.blind = None
+        if self._peripheral:
+            self._peripheral.disconnect()
+            self._peripheral = None
 
     def send(self, data: any, identifier: bytearray, wait_notification: bool = False) -> dict:
         while True:
             try:
-                if not self.blind:
+                if not self._peripheral:
                     self.connect()
 
                 message = bytearray({0x9a}) + bytearray({identifier}) + bytearray({len([data])}) + bytearray([data])
                 message += self._calculate_checksum(data=message)
 
-                if wait_notification and self.blind.waitForNotifications(10):
+                if wait_notification and self._peripheral.waitForNotifications(10):
                     pass
 
                 return self.characteristic.write(message)
             except btle.BTLEDisconnectError as error:
                 self.disconnect()
-                if not self.retry:
+                if not self._retry:
                     raise error
                 else:
                     self.connect()
