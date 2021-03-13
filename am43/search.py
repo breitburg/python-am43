ee5812ed547ae5e4200858585c10a83c787b489b
breitburg <me@breitburg.com>
2021-03-14 00:40:51 +0300

    Fixed lower case MAC address bug

--- am43/search.py
+++ am43/search.py
@@ -4,7 +4,7 @@ from bluepy import btle
 
 def search(*addresses: list, auto_connect: bool = True, retry: bool = True, device: int = 0) -> any:
     blinds = [Blind(device=device, auto_connect=auto_connect, retry=retry) for device in btle.Scanner(device).scan() if
-              device.addr in addresses]
+              device.addr in [address.lower() for address in addresses]]
 
     if not blinds:
         raise IndexError('there is no blinds were found')
