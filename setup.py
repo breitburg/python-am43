40c64cb3da45a5bc6458869758488d0c8935daee
Erno Kuvaja <jokke@ilujo.net>
2022-05-21 17:58:44 +0100

    remove dependency of munch

--- setup.py
+++ setup.py
@@ -13,6 +13,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    install_requires=['bluepy>=1.3.0', 'munch>=2.5.0'],
+    install_requires=['bluepy>=1.3.0'],
     python_requires='>=3.6'
 )
